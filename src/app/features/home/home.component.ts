3b7a92cde852fbf66a8f6b63bd7699334fd98d4e
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2025-04-15 12:00:14 +0400

    After programatically navigating

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MContainerComponent } from '../../m-framework/components/m-container/m-container.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router'; // Step 1: Import the service/provider 
 
 @Component({
   selector: 'app-home',
@@ -12,8 +13,17 @@ import { FormsModule } from '@angular/forms';
 })
 export class HomeComponent {
   
-  constructor() 
+  constructor(public router: Router) // Step 2: Inject the service - Depend. Injection 
   {
     
+  }
+  goToFeature1(){
+    this.router.navigateByUrl("/feature1"); // Step 3: Use the provider/service 
+  }
+  goToFeature2(){
+    this.router.navigateByUrl("/feature2");
+  }
+  goToFeature3(){
+    this.router.navigateByUrl("/feature3");
   }
 }
