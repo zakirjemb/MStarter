7a6efc611425bc16b96e4ee0784273ab546fc567
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2025-04-22 12:19:15 +0400

    ii

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,3 +1,11 @@
+
+// Run the app
+// Add another light sensor to be shown on a different canvasjs chart
+// Modify your implementation to be shown on the same canvasjs chart (one chart, two lines)
+// Modify your implementation to show three lines on the same chart. Light1, light2, their average
+// Add a slider to control a LED's intesity. 
+
+
 import { Component } from '@angular/core';
 import { MContainerComponent } from '../../m-framework/components/m-container/m-container.component';
 import { CommonModule } from '@angular/common';
