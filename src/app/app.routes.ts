256a11d5cfad0679dd48f7ceea1245af97263862
Mohammed Ghazal <drghazalm@gmail.comI love bananas>
2025-04-23 13:29:05 +0400

    sss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
-
+import { Feature1Component } from './feature1/feature1.component';
 export const routes: Routes = [
-    {path:'', component:HomeComponent}
+    {path:'', component:HomeComponent},
+    {path:'feature1', component:Feature1Component}
 ];
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,9 +1,10 @@
 export const environment = {
-    apiKey: "AIzaSyDVNNWlgk_Gb3IkmlGHQtzYD2I3cVrXofA",
-    authDomain: "ghazal1-31047.firebaseapp.com",
-    projectId: "ghazal1-31047",
-    storageBucket: "ghazal1-31047.firebasestorage.app",
-    messagingSenderId: "884604082913",
-    appId: "1:884604082913:web:9adcddc2361854c45bf5a0",
-    measurementId: "G-F039MWV4HN"
+    apiKey: "AIzaSyB132q5WSNcLcoL7sFr4AITgFL2YUD3JWc",
+    authDomain: "sideeffectsapp.firebaseapp.com",
+    databaseURL: "https://sideeffectsapp-default-rtdb.firebaseio.com",
+    projectId: "sideeffectsapp",
+    storageBucket: "sideeffectsapp.firebasestorage.app",
+    messagingSenderId: "628029335425",
+    appId: "1:628029335425:web:9e47b02e1c3bf9ec55aa80",
+    measurementId: "G-786VKV77F3"
   };
