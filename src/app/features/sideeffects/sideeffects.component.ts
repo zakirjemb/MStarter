2ad6de7cea4b194c33a4671db982a4cfaeb1e4f0
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2025-04-24 12:37:59 +0400

    Final SideEffects

--- src/app/features/sideeffects/sideeffects.component.ts
+++ src/app/features/sideeffects/sideeffects.component.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
+import { MContainerComponent } from "../../m-framework/components/m-container/m-container.component";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { getDatabase, ref, set, push, remove, onValue, get } from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-sideeffects',
   standalone: true,
-  imports: [],
+  imports: [MContainerComponent,CommonModule,FormsModule,CanvasJSAngularChartsModule,HttpClientModule],
   templateUrl: './sideeffects.component.html',
   styleUrl: './sideeffects.component.css'
 })
 export class SideeffectsComponent {
+  medicine: string;
+  chart: any; 
+  chartOptions: any; 
+  db: any;
+  epc: string;
+  sideeffects: any[];
+
+  getChartInstance(chart: object) {
+    this.chart = chart;
+  }
+  constructor(public httpClient: HttpClient){
+    this.medicine = "";
+    const firebaseApp = initializeApp(environment);
+    this.db = getDatabase(firebaseApp);
+    this.epc = "";
+    this.sideeffects = [];
+    this.chartOptions = {
+      theme: "light2",
+      animationEnabled: true,
+      title: { text: " Side Effects vs Frequency"},
+      axisX: {labelAngle: -90}
+    }
+
+  }
+  findSideEffects(){
+      console.log("/"+this.medicine);
+      get(ref(this.db,"/"+this.medicine))
+        .then( (data)  => {
+          this.epc = data.val();
+          this.httpClient.get("https://api.fda.gov/drug/event.json?search=patient.drug.openfda.pharm_class_epc:" + this.epc + "&count=patient.reaction.reactionmeddrapt.exact")
+              .subscribe((data:any)=> {
+                for(let i = 0 ; i < 10; i++)
+                  this.sideeffects.push({label:data.results[i].term, y:data.results[i].count });
+                this.chartOptions.data = [{type:"column",  dataPoints: this.sideeffects}];
+                this.chart.render();
+
+              });
+          
+
+
+        })
+        .catch((error) => console.log(error));
+    
+  }
+  
 
 }
