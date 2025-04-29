c8d557b2bf3e951e1d650f0ebe27523386a1dc84
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2025-04-29 12:39:55 +0400

    Qiba 2

--- src/app/features/qiblafinder/qiblafinder.component.ts
+++ src/app/features/qiblafinder/qiblafinder.component.ts
@@ -2,19 +2,50 @@ import { Component } from '@angular/core';
 import { MContainerComponent } from "../../m-framework/components/m-container/m-container.component";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 @Component({
   selector: 'app-qiblafinder',
   standalone: true,
-  imports: [MContainerComponent,FormsModule,CommonModule],
+  imports: [MContainerComponent, FormsModule, CommonModule, HttpClientModule],
   templateUrl: './qiblafinder.component.html',
-  styleUrl: './qiblafinder.component.css'
+  styleUrl: './qiblafinder.component.css',
 })
 export class QiblafinderComponent {
-  city: string | null = null;
+  lat: number;
+  lng: number;
+  city: string;
   angle: number | null = null;
-  constructor(){
-  }
-  findQibla(){
+  googleApiKey: string = 'AIzaSyBKlRRlfmxLvZl96bN-kh5gXBhISBM7_bY';
 
+  readonly MakkaLatitude: number = 21.4225;
+  readonly MakkaLongitude: number = 39.8262;
+
+  constructor(public httpClient: HttpClient) {
+    this.city = '';
+    this.lat = -1;
+    this.lng = -1;
+  }
+  findQibla() {
+    const url: string = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(this.city)}&key=${this.googleApiKey}`;
+    this.httpClient.get(url).subscribe((data: any) => {
+      this.lat = data.results[0].geometry.location.lat;
+      this.lng = data.results[0].geometry.location.lng;
+      this.getQiblaDirection(this.lat, this.lng);
+    });
+  }
+
+  getQiblaDirection(lat:number, lng:number) {
+    const dLon = ((this.MakkaLongitude - lng) * Math.PI) / 180;
+    lat *= Math.PI / 180;
+    const qibla =
+      (Math.atan2(
+        Math.sin(dLon),
+        Math.cos(lat) * Math.tan((this.MakkaLatitude * Math.PI) / 180) -
+          Math.sin(lat) * Math.cos(dLon)
+      ) *
+        180) /
+      Math.PI;
+    this.angle = (qibla + 360) % 360;
   }
 }
