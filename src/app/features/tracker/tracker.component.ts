425a6d874d2364c807fb5f72541103c97a416a59
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2025-05-06 11:41:56 +0400

    sssa

--- src/app/features/tracker/tracker.component.ts
+++ src/app/features/tracker/tracker.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MContainerComponent } from "../../m-framework/components/m-container/m-container.component";
-
+import { GoogleMap } from '@angular/google-maps';
 @Component({
   selector: 'app-tracker',
   standalone: true,
-  imports: [MContainerComponent],
+  imports: [MContainerComponent,GoogleMap],
   templateUrl: './tracker.component.html',
   styleUrl: './tracker.component.css'
 })
@@ -12,6 +12,8 @@ export class TrackerComponent {
 
   lat: number;
   lng: number; 
+  center: google.maps.LatLngLiteral =  {lat: 0, lng: 0};
+  zoom: number = 4;
 
   constructor(){
     this.lat = 0; 
@@ -19,7 +21,11 @@ export class TrackerComponent {
     navigator.geolocation.getCurrentPosition((data)=>{
       this.lat = data.coords.latitude;
       this.lng = data.coords.longitude;
+      this.center = {lat:this.lat , lng: this.lng};
     });
 
+  }
+  showWhereTheUserClicked(event: google.maps.MapMouseEvent){
+      console.log(event.latLng?.toJSON());
   }
 }
