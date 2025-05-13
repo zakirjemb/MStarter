8921ea8038d8a247edada46bb8e4725a7b103800
CEN333-Spring23-24 <CEN333-Spring23-24@outlook.com>
2025-05-13 12:04:57 +0400

    ssadas

--- src/app/features/trackerdev/trackerdev.component.ts
+++ src/app/features/trackerdev/trackerdev.component.ts
@@ -1,34 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MContainerComponent } from "../../m-framework/components/m-container/m-container.component";
-import { GoogleMap } from '@angular/google-maps';
+import { GoogleMap,MapMarker } from '@angular/google-maps';
+import { CommonModule } from '@angular/common';
+interface InformedMarker{
+  position: google.maps.LatLngLiteral;
+  title: string; 
+  subtitle: string; 
+  info: string; 
+};
+
+
+
+
 @Component({
   selector: 'app-trackerdev',
   standalone: true,
-  imports: [MContainerComponent,GoogleMap],
+  imports: [MContainerComponent,GoogleMap,MapMarker,CommonModule],
   templateUrl: './trackerdev.component.html',
   styleUrl: './trackerdev.component.css'
 })
-export class TrackerdevComponent {
+export class TrackerdevComponent implements OnInit{
 
-  currentlocation: google.maps.LatLngLiteral;
-  clicklocation:   google.maps.LatLngLiteral;
+  currentlocation: google.maps.LatLngLiteral | void;
+  clicklocation:   google.maps.LatLngLiteral | undefined;
   mapcenter:       google.maps.LatLngLiteral;
-  zoom: number; 
+  zoom: number;
+  informedMakerlist: InformedMarker[]; 
 
   constructor(){
     this.currentlocation = {lat: 0, lng: 0};
     this.mapcenter = {lat: 0, lng: 0};
     this.clicklocation = {lat: 0, lng: 0};
     this.zoom = 4;
-
-    navigator.geolocation.getCurrentPosition((data)=>{
-      this.currentlocation.lat = data.coords.latitude;
-      this.currentlocation.lng = data.coords.longitude;
-    })
+    this.informedMakerlist = [];
   }
-  showMe(event:any){
-    this.clicklocation.lat = event.latLng.lat();
-    this.clicklocation.lng = event.latLng.lng();
+  async ngOnInit() {
+      this.currentlocation = await this.getCurrentLocation().catch((error)=>{console.log(error)});
+  }
+  
+  async showMe(event:google.maps.MapMouseEvent){
+    this.clicklocation = event.latLng?.toJSON();
+  }
+  addMarker(event: google.maps.MapMouseEvent){
+    const locationClicked = event.latLng?.toJSON();
+    if(locationClicked)
+      this.informedMakerlist.push({position:locationClicked,title:"Title Goes Here",subtitle:"Subtitle Goes Here",info:"Info Goes Here"});
+  
+
+  }
+
+  getCurrentLocation(): Promise<{lat: number, lng: number}>{
+    return new Promise((resolve,reject)=>{
+      navigator.geolocation.getCurrentPosition((data)=>{
+        const location = {lat: data.coords.latitude, lng: data.coords.longitude};
+        resolve(location);
+      },(error)=>{
+        reject(error);
+      })
+    });
   }
 
 }
